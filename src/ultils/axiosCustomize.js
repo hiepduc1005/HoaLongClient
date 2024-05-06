689be7cf10b84ceef5d1815610a16121ebfba838
duc hiep <hiep100524@gmail.com>
2024-05-07 00:48:10 +0700

    update new

--- src/ultils/axiosCustomize.js
+++ src/ultils/axiosCustomize.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL: 'https://hoalong.onrender.com/'
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://hoalong.onrender.com/'
+    // baseURL: 'http://localhost:8080/'
 })
 
 export default instance;
